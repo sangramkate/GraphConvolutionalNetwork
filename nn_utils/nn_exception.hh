6e294e3fe6bd45d03618e310dfafe6ae2a06957c
sangramkate <46626586+sangramkate@users.noreply.github.com>
2020-11-21 11:38:22 -0600

    Exception generation code

---
+++ nn_utils/nn_exception.hh
@@ -0,0 +1,26 @@
+#pragma once
+#include <exception>
+#include <iostream>
+
+class NNException : std::exception{
+private:
+    const char* exception_msg;
+    
+public:
+    NNException(const char* exception_msg):
+        exception_msg(exception_msg)
+    { }
+    
+    virtual const char* what() const throw()
+    {
+        return exception_msg;
+    }
+       
+    static void throwIfDeviceErrorOccurred( const char* exception_msg){
+        cudaError_t error = cudaGetLastError();
+        if(error != cudaSuccess){
+            std::cerr << error << ":" << exception_msg;
+            throw NNException(exception_msg);
+        }
+    }
+}
