f4cc67eb7fa665bbb1c54afef235807601ea45f4
sangramkate <46626586+sangramkate@users.noreply.github.com>
2020-11-22 08:34:32 -0600

    Linear layer functions

--- Layers/linear_layer.cu
+++ Layers/linear_layer.cu
@@ -42,5 +42,156 @@ __global__ void linearLayerBackprop( float* W, float* dZ, float*dA,
 		    dA[row * dA_x_dim + col] = dA_value;
 	  }
 }
-    
+
+__global__ void linearLayerUpdateWeights(  float* dZ, float* A, float* W,
+										   int dZ_x_dim, int dZ_y_dim,
+										   int A_x_dim, int A_y_dim,
+										   float learning_rate) {
+
+	int col = blockIdx.x * blockDim.x + threadIdx.x;
+	int row = blockIdx.y * blockDim.y + threadIdx.y;
+
+	// A is treated as transposed
+	int W_x_dim = A_y_dim;
+	int W_y_dim = dZ_y_dim;
+
+	float dW_value = 0.0f;
+
+	if (row < W_y_dim && col < W_x_dim) {
+		for (int i = 0; i < dZ_x_dim; i++) {
+			dW_value += dZ[row * dZ_x_dim + i] * A[col * A_x_dim + i];
+		}
+		W[row * W_x_dim + col] = W[row * W_x_dim + col] - learning_rate * (dW_value / A_x_dim);
+	}
 }
+
+__global__ void linearLayerUpdateBias(  float* dZ, float* b,
+										int dZ_x_dim, int dZ_y_dim,
+										int b_x_dim,
+										float learning_rate) {
+	int index = blockIdx.x * blockDim.x + threadIdx.x;
+
+	if (index < dZ_x_dim * dZ_y_dim) {
+		int dZ_x = index % dZ_x_dim;
+		int dZ_y = index / dZ_x_dim;
+		atomicAdd(&b[dZ_y], - learning_rate * (dZ[dZ_y * dZ_x_dim + dZ_x] / dZ_x_dim));
+	}
+}
+
+LinearLayer::LinearLayer(std::string name, Shape W_shape):
+    W(W_shape),b(W_shape.y,1)
+{
+    this->name = name;
+    b.allocateMemory();
+    w.allocateMemory();
+    initializeBiasWithZeros();
+    initializeWeightsRandomly();
+}
+
+LinearLayer::~LinearLayer()
+{ }
+
+void LinearLayer::initializeWeightsRandomly(){
+    std::default_ranodm_engine generator;
+    std::normal_distribution<float> normal_distribution(0.0, 1.0);
+	
+    for(int x = 0; x < W.shape.x;x++){
+	for(int y =0; y< W.shape.y;y++){
+	     W[y* W.shape.x * + x] = normal_distribution(generator) * weight_initi_threshold;	
+	}
+    }
+    W.copyHostToDevice();
+}
+
+void LinearLayer::initializeBiasWithZeros() {
+	for (int x = 0; x < b.shape.x; x++) {
+		b[x] = 0;
+	}
+
+	b.copyHostToDevice();
+}
+
+Matrix& LinearLayer::forward(Matrix& A){
+    assert(W.shape.x = A.shape.y);
+    this->A = A;
+    Shape Z_shape(A.shape.x,W.shape.y)
+    Z.allocateMemoryIfNotAllocated(Z_shape);
+    ComputeAndStoreLayerOutoput(A);
+    NNException::throwIfDeviceErrorsOccurred("Cannot perform Linear Layer forward propagation");
+    
+    return Z;
+	
+}
+void LinearLayer::computeAndStoreLayerOutput(Matrix& A) {
+dim3 block_size(8, 8);
+dim3 num_of_blocks(	(Z.shape.x + block_size.x - 1) / block_size.x,
+					(Z.shape.y + block_size.y - 1) / block_size.y);
+linearLayerForward<<<num_of_blocks, block_size>>>( W.data_device.get(),
+				                   A.data_device.get(),
+						   Z.data_device.get(),
+						   b.data_device.get(),
+						   W.shape.x, W.shape.y,
+						   A.shape.x, A.shape.y);
+}
+
+Matrix& LinearLayer::backprop(Matrix& dZ, float learning_rate) {
+	dA.allocateMemoryIfNotAllocated(A.shape);
+
+	computeAndStoreBackpropError(dZ);
+	NNException::throwIfDeviceErrorsOccurred("Cannot perform back propagation.");
+
+	updateBias(dZ, learning_rate);
+	NNException::throwIfDeviceErrorsOccurred("Cannot perform bias update.");
+
+	updateWeights(dZ, learning_rate);
+	NNException::throwIfDeviceErrorsOccurred("Cannot perform weights update.");
+
+	return dA;
+}
+
+void LinearLayer::computeAndStoreBackpropError(Matrix& dZ) {
+	dim3 block_size(8, 8);
+	dim3 num_of_blocks((A.shape.x + block_size.x - 1) / block_size.x,(A.shape.y + block_size.y - 1) / block_size.y);
+	linearLayerBackprop<<<num_of_blocks, block_size >>> ( W.data_device.get(),
+							     dZ.data_device.get(),
+							     dA.data_device.get(),
+							     W.shape.x, W.shape.y,
+							     dZ.shape.x, dZ.shape.y);
+}
+
+void LinearLayer::updateWeights(Matrix& dZ, float learning_rate) {
+	dim3 block_size(8, 8);
+	dim3 num_of_blocks((W.shape.x + block_size.x - 1) / block_size.x,(W.shape.y + block_size.y - 1) / block_size.y);
+	linearLayerUpdateWeights<<<num_of_blocks, block_size>>>(dZ.data_device.get(),
+								A.data_device.get(),
+								W.data_device.get(),
+								dZ.shape.x, dZ.shape.y,
+								A.shape.x, A.shape.y,
+								learning_rate);
+}
+
+void LinearLayer::updateBias(Matrix& dZ, float learning_rate) {
+	dim3 block_size(256);
+	dim3 num_of_blocks( (dZ.shape.y * dZ.shape.x + block_size.x - 1) / block_size.x);
+	linearLayerUpdateBias<<<num_of_blocks, block_size>>>(dZ.data_device.get(),
+							     b.data_device.get(),
+							     dZ.shape.x, dZ.shape.y,
+							     b.shape.x, learning_rate);
+}
+
+int LinearLayer::getXdim() const {
+	return W.shape.x;
+}
+
+int LinearLayer::getYdim() const {
+	return W.shape.y;
+}
+
+Matrix LinearLayer::getWeightsMatrix() const {
+	return W;
+}
+
+Matrix LinearLayer::getBiasVector() const {
+	return b;
+}
+	    
