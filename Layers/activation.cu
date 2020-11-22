b84980ecd9c4618a142d083c6071d7b7a7e88ba6
sangramkate <46626586+sangramkate@users.noreply.github.com>
2020-11-22 11:34:51 -0600

    Update activation.cu

--- Layers/activation.cu
+++ Layers/activation.cu
@@ -1,8 +1,7 @@
-#include "relu_activation.hh"
+#include "activation.hh"
 #include "../nn_utils/nn_exception.hh"
 
-__global__ void reluActivationForward(float* Z, float* A,
-									  int Z_x_dim, int Z_y_dim) {
+__global__ void ReluActivationForward(float* Z, float* A, int Z_x_dim, int Z_y_dim) {
 
 	int index = blockIdx.x * blockDim.x + threadIdx.x;
 
@@ -11,8 +10,7 @@ __global__ void reluActivationForward(float* Z, float* A,
 	}
 }
 
-__global__ void reluActivationBackprop(float* Z, float* dA, float* dZ,
-									   int Z_x_dim, int Z_y_dim) {
+__global__ void ReluActivationBackprop(float* Z, float* dA, float* dZ, int Z_x_dim, int Z_y_dim) {
 
 	int index = blockIdx.x * blockDim.x + threadIdx.x;
 
@@ -39,8 +37,7 @@ Matrix& ReLUActivation::forward(Matrix& Z) {
 	dim3 block_size(256);
 	dim3 num_of_blocks((Z.shape.y * Z.shape.x + block_size.x - 1) / block_size.x);
 
-	reluActivationForward<<<num_of_blocks, block_size>>>(Z.data_device.get(), A.data_device.get(),
-														 Z.shape.x, Z.shape.y);
+	reluActivationForward<<<num_of_blocks, block_size>>>(Z.data_device.get(), A.data_device.get(), Z.shape.x, Z.shape.y);
 	NNException::throwIfDeviceErrorsOccurred("Cannot perform ReLU forward propagation.");
 
 	return A;
@@ -51,9 +48,7 @@ Matrix& ReLUActivation::backprop(Matrix& dA, float learning_rate) {
 
 	dim3 block_size(256);
 	dim3 num_of_blocks((Z.shape.y * Z.shape.x + block_size.x - 1) / block_size.x);
-	reluActivationBackprop<<<num_of_blocks, block_size>>>(Z.data_device.get(), dA.data_device.get(),
-													      dZ.data_device.get(),
-														  Z.shape.x, Z.shape.y);
+	reluActivationBackprop<<<num_of_blocks, block_size>>>(Z.data_device.get(), dA.data_device.get(),dZ.data_device.get(), Z.shape.x, Z.shape.y);
 	NNException::throwIfDeviceErrorsOccurred("Cannot perform ReLU back propagation");
 
 	return dZ;
