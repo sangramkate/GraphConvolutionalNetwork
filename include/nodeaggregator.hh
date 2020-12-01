020dc05eec9cbdf87e471a269c408a2df61a9853
Divija Gogineni <dg38782@tuxedo.ices.utexas.edu>
2020-12-01 01:12:21 -0600

    Adding nodeaggregator layer

--- include/nodeaggregator.hh
+++ include/nodeaggregator.hh
@@ -1,1 +1,17 @@
+#pragma once
 
+#include "nn_layer.hh"
+
+class ReLUActivation : public NNLayer {
+private:
+	Matrix A;
+	Matrix Z;
+	Matrix dZ;
+
+public:
+	NodeAgregator(std::string name,Shape W_shape, float* nnz_data, int* row, int*col, int feature_size);
+	~NodeAggregator();
+
+	Matrix& Forward((Matrix& A, float* nnz_data, int* row, int* col));
+	Matrix& Backprop(Matrix& dZ, float* nnz_data, int* row, int* col);
+};
